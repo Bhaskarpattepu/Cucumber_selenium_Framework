668cd88b72cf0d5d6c860a4d30205fc0ade802c7
bhaskarpattepu <Bhaskar.pattepu123@gmail.com>
2025-10-24 16:07:14 +0530

    some cleanup

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -9,7 +9,7 @@ import org.testng.annotations.DataProvider;
         features = "src/test/java/features",
         glue="stepDefinitions",
         monochrome = true,
-        tags = "@PlaceOrder",
+        tags = "@PlaceOrder or @OffersPage",
         plugin ={
                 "html:target/cucumber.html","json:target/cucumber.json",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
